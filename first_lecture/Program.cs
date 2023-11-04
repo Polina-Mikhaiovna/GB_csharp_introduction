7b4063e233ab1d7d9a1b353906719cf6290f2393
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2023-11-04 18:42:13 +0300

    Upgrade of The Sierpinski triangle

--- first_lecture/Program.cs
+++ first_lecture/Program.cs
@@ -1,10 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿Console.Clear();
 
-// Console.Clear();
-
-// Вершины треугольника х(1,70)
-//						y(1,20)
+// Вершины треугольника a(1,100)
+//						b(50,1)
+//						c(100,30)
 int count = 0;
 
 int ax = 1, ay = 30;
@@ -12,13 +10,6 @@ int bx = 50, by = 1;
 int cx = 100, cy = 30;
 
 int tempX = ax, tempY = ay;
-
-// Console.SetCursorPosition(ax, ay);
-// Console.WriteLine("#");
-// Console.SetCursorPosition(bx, by);
-// Console.WriteLine("#");
-// Console.SetCursorPosition(cx, cy);
-// Console.WriteLine("#");
 
 while (count < 10000) {
 	int random_top = new Random().Next(1,4);
@@ -44,4 +35,5 @@ Console.SetCursorPosition(ax, ay);
 for(int i = 1; i <= cx; i++) {
 	Console.Write("_");
 }
+
 Console.WriteLine("\n");
