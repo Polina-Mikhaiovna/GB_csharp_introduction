14add213750d701ae26d9666dcc724e6053fc1f0
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2023-11-06 20:46:06 +0300

    задача с изменением заданных символов в строке, без общения с пользователем

--- second_lecture/Program.cs
+++ second_lecture/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание: заменить в cтроке text	" " на "|"
+//									"c" на "C"
+//
+
+string text = "Console.WriteLine [main cf9835e] add folder sec_lec\n7 files changed, 162 insertions(+)";
+char old_symbol;
+char new_symbol;
+old_symbol = ' ';
+new_symbol = '|';
+string result_string = Replase(text, old_symbol, new_symbol);
+
+
+string Replase(string origin_string, char oldV, char newV){
+	
+	string changed_string = String.Empty;
+	int length = origin_string.Length;
+	
+	for (int i = 0; i < length; i++) {
+		if (origin_string[i] == oldV) {
+			changed_string = changed_string	+ $"{newV}";
+			Console.WriteLine(changed_string);
+			break;
+		}
+		else changed_string = changed_string + $"{origin_string[i]}";
+	}
+	return changed_string;
+}
+
+
+Console.WriteLine(result_string);
