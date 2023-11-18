87a02b472b726092f8d2bd71c42d1dd523621ac0
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2023-11-18 19:56:29 +0300

    More Recurcion - write numbers from a to b; a raise to power n

--- seventh_lecture/Program.cs
+++ seventh_lecture/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Собрать строку с числами от а до b, a <= b
+// Циклом
 
-string NumbersString(int a, int b)
+string NumbersFor(int a, int b)
 {
 	string result = String.Empty;
 	for(int i = a; i <= b; i++)
@@ -10,17 +11,41 @@ string NumbersString(int a, int b)
 	return result;
 }
 
-Console.WriteLine($"{NumbersString(4, 8)}");
+Console.WriteLine($"{NumbersFor(4, 8)}");
 
-
+// Собрать строку с числами от а до b, a <= b
+// С помощью рекурсии
 
 string res = String.Empty;
 
-string List(int a, int b)
+string NumbersRec(int a, int b)
 {
-	if(a <= b) return $"{a} " + List(a+1, b);
-	else return String.Empty;
+	if(a < b) return $"{a} " + NumbersRec(a+1, b);
+	else return $"{b}";
 
 }
 
-Console.WriteLine(List(1, 5));
+Console.WriteLine(NumbersRec(1, 5));
+
+// Вычислить а в степени n
+// Циклом
+
+int PowerFor(int a, int n)
+{
+	int result = 1;
+	for(int i = 1; i <= n; i++) result *= a;
+	return result;
+}
+
+Console.WriteLine(PowerFor(5,3));
+
+// Вычислить а в степени n
+// Рекурсией
+
+int PowerRec(int a, int n)
+{
+	if (n != 0) { return PowerRec(a, n-1) * a;}
+	else return 1;
+}
+
+Console.WriteLine(PowerRec(4,3));
