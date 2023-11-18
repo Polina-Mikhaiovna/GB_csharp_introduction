e7f906cae5378b902204a70e742ba372e92863f5
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2023-11-18 18:29:59 +0300

    More Recurcion

---
+++ seventh_lecture/Program.cs
@@ -0,0 +1,26 @@
+﻿// Собрать строку с числами от а до b, a <= b
+
+string NumbersString(int a, int b)
+{
+	string result = String.Empty;
+	for(int i = a; i <= b; i++)
+	{
+		result += $"{i} ";
+	}
+	return result;
+}
+
+Console.WriteLine($"{NumbersString(4, 8)}");
+
+
+
+string res = String.Empty;
+
+string List(int a, int b)
+{
+	if(a <= b) return $"{a} " + List(a+1, b);
+	else return String.Empty;
+
+}
+
+Console.WriteLine(List(1, 5));
