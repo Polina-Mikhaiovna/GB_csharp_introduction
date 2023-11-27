3eaef40ca7eaaedb50c0de82efa66b3415a3a47f
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2023-11-27 16:08:54 +0300

    Task 64 is done

--- certification/task64/Program.cs
+++ certification/task64/Program.cs
@@ -3,17 +3,43 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
-int start = ReadConsoleInt("Введите желаемое начало диапазона чётных натуральных чисел:");
+
+int start = ReadConsoleInt("Введите желаемое начало диапазона чётных натуральных чисел: ");
 int end = ReadConsoleInt("Введите желаемый конец диапазона чётных натуральных чисел: ");
 
-void EvenInteger()
+if (start == -1 || end == -1) Console.WriteLine("Вы ввели не натуральное число, " + 
+												"перезапустите программу и попробуйте снова =)");
+else
 {
-	
+	if (start > end) Change(start, end);
+	if (start == end) Console.WriteLine("Увеличте диапазон чисел и попробуйте снова!");
+	else
+	{
+		if ((start % 2) != 0) start += 1;
+		Console.Write(start);
+		Console.WriteLine(PrintEvenInteger(start + 2, end));
+	}
 }
-
 
 int ReadConsoleInt(string message)
 {
 	Console.Write(message);
-	return Convert.ToInt32(Console.ReadLine());
+	int integerNumber = 0;
+	var stringValue = Console.ReadLine();
+	if (int.TryParse(stringValue, out integerNumber)) return integerNumber;
+	else return -1;
+
+}
+
+string PrintEvenInteger(int a, int b)
+{
+	if(a <= b) return $" {a}" + PrintEvenInteger(a+2, b);
+	else return $"{'\0'}";
+
+}
+
+void Change(int a, int b)
+{
+	start = b;
+	end = a;
 }
---
+++ certification/task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int start = ReadConsoleInt("Введите желаемое начало диапазона чётных натуральных чисел:");
+int end = ReadConsoleInt("Введите желаемый конец диапазона чётных натуральных чисел: ");
+PrintInt(start);
+
+int ReadConsoleInt(string message)
+{
+	Console.Write(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintInt(int start)
+{
+	if (start <= end) Console.Write($" {PrintInt(start+1)}");
+}
+// Console.WriteLine($"{start} {end}");
